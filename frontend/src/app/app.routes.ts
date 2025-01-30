baab1e3dbdb7d2f4fcf6975ddb38b4175df2fe0b
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2025-01-30 18:55:31 -0300

    Criado o modelo da página inicial + rota para o quiz

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuizComponent } from './pages/quiz/quiz.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'quiz', component: QuizComponent }
+];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, { 
+  providers: [provideRouter(routes)]
+  }).catch((err) => console.error(err));
